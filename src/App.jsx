9822a050704eec3057ddd6630971019146144399
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-08-26 19:22:24 +0530

    Currency converter bgImg setting

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-fulll h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: `url(https://media.istockphoto.com/id/1414200026/photo/stack-of-money-coin-and-laptop-computer-with-trading-graph-financial-investment-concept-with.jpg?s=612x612&w=0&k=20&c=tqQqK6p3QoOiO7M3RAO5p98ajHlFnnQnw0Ru2lPz6eA=)`}}>
+
+    </div>
+  )
+}
+
+export default App
