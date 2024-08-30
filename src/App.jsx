ec714a8d2b437a8e5266f69b1236ba29124b150d
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-08-30 21:58:18 +0530

    Final work with CurrencyConverter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
-import {InputBox} from './components/index'
+import {InputBox} from './components/index.js'
 
 function App() {
   const [amount, setAmount] = useState(0)
@@ -17,7 +17,7 @@ function App() {
   }
 
   const convert = () => {
-    setConvertedAmount(amount * currencyInfo[to])
+    setConvertedAmount((amount * currencyInfo[to]).toFixed(2))
   }
 
   const currencyInfo = useCurrencyInfo(from)
@@ -31,14 +31,35 @@ function App() {
             e.preventDefault()
             convert()
           }}>
-            <InputBox
-            label='from'
-            amount={amount}
-            currencyOptions={options}
-            onCurrencyChange={(currency) => setFrom(currency)}
-            onAmountChange={(amount) => setAmount(amount)}
-            selectedCurrency={from}
-            />
+            <div>
+              <InputBox
+              label='From'
+              amount={amount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              onAmountChange={(amount) => setAmount(amount)}
+              selectedCurrency={from}
+              />
+            </div>
+            <div className='relative w-full h-0.5'> 
+              <button 
+              className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
+              onClick={swap}
+              >Swap</button>
+            </div>
+            <div className='w-full mb-1'>
+              <InputBox 
+              label="To"
+              currencyOptions={options}
+              amount={convertedAmount} 
+              onCurrencyChange={(currency) => setTo(currency)}
+              selectedCurrency={to}
+              amountDisabled
+              />
+            </div>
+            <button type='submit'
+            className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg'
+            >Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
           </form>
         </div>
       </div>
